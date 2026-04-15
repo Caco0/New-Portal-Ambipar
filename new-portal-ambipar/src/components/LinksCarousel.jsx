ee3737c0e753cd29894b6dccab1ac8d40e6b04d1
Caco0 <cacotpds@gmail.com>
2026-04-15 11:23:45 -0300

    Update a new link on carousel list link

--- new-portal-ambipar/src/components/LinksCarousel.jsx
+++ new-portal-ambipar/src/components/LinksCarousel.jsx
@@ -23,6 +23,7 @@ export default function LinksCarousel() {
         { title: "🙋‍♀️ Feedz", url: "https://app.feedz.com.br", color: "border-blue-500", description: "Comunidade Ambipar Limeira e Andradas" },
         { title: "🔧 Chamado TI", url: "http://10.0.0.251/glpi/front/central.php", color: "border-yellow-500", description: "Painel de chamados de TI" },
         { title: "🌐 Nosso Site", url: "https://fundicaomagma.com.br/", color: "border-purple-500", description: "Nossa página Web" },
+        { title: "🌐 Workflow Ambipar", url: "https://ambipar.wtmh.com.br/login", color: "border-black-500", description: "Nossa página Web" },
         { title: "🌐 Sistema Sygecom", url: "https://cloud.sygecom.com.br/sagi_magma.html", color: "border-green-500", description: "Processos sistema Sygecom" },
         { title: "🚚 Logística", url: "/logisticalinks", color: "border-pink-500", description: "Links de reatreamento de Logística." },
         { title: "💰 Financeiro", url: "/financeiroLinks", color: "border-green-500", description: "Links Financeiros." },
